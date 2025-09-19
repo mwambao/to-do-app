f14a76dcfa49a4ad6978966db32e28a30717fd95
Duncan <duncan.owino@gmail.com>
2025-09-19 11:12:25 +0100

    comments

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-/* import modules Ref: npmjs.com*/
-const express = require("express");
-const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
-const Task = require("./models/Task")
+// import required modules Ref: npmjs.com
+const express = require("express"); // Express framework for building webapps
+const mongoose = require("mongoose"); //Mongoose for MongoDB modelling
+const bodyParser = require("body-parser"); // middleware to parse form data
+const Task = require("./models/Task") // Our Task model (schema)
